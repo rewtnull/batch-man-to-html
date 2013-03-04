7f2bdbf57f944b7961b86dabbcf08911690d9dac
rewtnull <marcus.hoffren@gmail.com>
2013-03-04 01:52:53 +0100

    version bump

--- bm2h.sh
+++ bm2h.sh
@@ -11,7 +11,7 @@
 source bm2h.conf
 
 scrname="bm2h"
-scrver="0.3"
+scrver="0.4"
 scrauth="Marcus Hoffren"
 authnick="dMG/Up Rough"
 scrcontact="marcus.hoffren@gmail.com"
@@ -51,72 +51,63 @@ sanity() {
     [[ ! -d ${src_root} ]] && { echo -e "${src_root} - Directory \033[1mdoes not exist\033[m."; exit 1; }
     if [[ ! -d "${dst_root}" ]]; then
 	if (( ${#} == 0 )); then
-	    echo "${dst_root%/} - Destination directory does not exist."
+	    echo "${dst_root%/} - Destination directory does not exist." # Strip trailing /
 	    read -p "Do you want it to be created? [y/N]"
 	    [[ "${REPLY}" == "y" ]] && mkdir ${dst_root%/} || exit 1
 	fi
     fi
 }
 
+verbose_mode() {
+    case ${verbose} in
+	0) echo -e "$@" 1> /dev/null;;
+	1) echo -e "$@";;
+    esac
+}
+
 args() {
-    getopt_arg=$(getopt -n "${0##*/}" -o "Vhsv" -l "version,help,generate-stub,verbose" -- "${@}") || { usage; exit 1; }
-    echo "getopt_arg: $getopt_arg"
-    set -- ${getopt_arg}
-    echo "\$@: $@"
+    getopt_arg=$(getopt -o "Vhsv" -l "version,help,generate-stub,verbose" -n "${0##*/}" -- "${@}") || { usage; exit 1; }
+    eval set -- "${getopt_arg}"
     while (( ${#} > 0 )); do
-	case ${1} in
+	case "${1}" in
 	    -V|--version)
 				{ version; exit 0; };;
 	    -h|--help)
 				{ usage; exit 0; };;
 	    -s|--generate-stub)
-				gen_stub="1"
-				echo "STUB";;
+				(( ${gen_stub} == "1" )) && gen_stub="0" || gen_stub="1"
+				shift;;
 	    -v|--verbose)
-				verbose="1"
-				echo "VERBOSE";;
+				(( ${verbose} == "1" )) && verbose="0" || verbose="1"
+				shift;;
 	    --)
-				echo "end of getopt args"
 				shift
 				break;;
-	    *)
-				{ usage; exit 1; };;
 	esac
-	shift
     done
-}
-
-
-
-num_args() {
-echo "1: $1 2: $2 3: $3 4: $4 5: $5"
     case ${#} in
 	0)
-	    echo "zero"
-	    src_dirs=( $(echo "${src_root}${man_dirs}") )
-	    dst_dirs=( $(echo "${dst_root}") );;
+	    src_dirs=( $(echo "${src_root%/}${man_dirs%/}") )
+	    dst_dirs=( $(echo "${dst_root%/}") );;
 	1)
-	    echo "one"
-	    src_dirs=( $(echo "${1%/}${man_dirs}") ) # Strip trailing /
-	    [[ ! -d ${src_dirs} ]] && { echo "No man dirs found under ${src_dirs}"; exit 1; }
-	    dst_dirs=( $(echo "${dst_root}") );;
+	    [[ ! -d ${1} ]] && { echo "${1} - Source directory does not exist."; exit 1; }
+	    src_dirs=( $(echo "${1%/}${man_dirs%/}") )
+	    [[ ! -d ${src_dirs} ]] && { echo "No man directories found under ${src_dirs}"; exit 1; }
+	    dst_dirs=( $(echo "${dst_root%/}") );;
 	2)
-	    echo "two"
-	    src_dirs=( $(echo "${1%/}${man_dirs}") )
-	    dst_dirs=( $(echo "${2%/}") );;
-#	    if [[ ! -d ${dst_dirs} ]]; then
-#		echo "${2%/} - Destination directory does not exist."
-#		read -p "Do you want it to be created? [y/N]"
-#		[[ "${REPLY}" == "y" ]] && mkdir ${2%/} || exit 1
-#	    fi;;
+	    src_dirs=( $(echo "${1%/}${man_dirs%/}") )
+	    dst_dirs=( $(echo "${2%/}") )
+	    if [[ ! -d ${dst_dirs} ]]; then
+		echo "${2%/} - Destination directory does not exist."
+		read -p "Do you want it to be created? [y/N]"
+		[[ "${REPLY}" == "y" ]] && mkdir ${2%/} || exit 1
+	    fi;;
 	*)
 	    echo "${0##*/} - Wrong number of arguments."
 	    { usage; exit 1; };;
     esac
-echo "src 1: $src_dirs"
-echo "dst 1: $dst_dirs"
-[[ ! -d ${src_dirs} ]] && { echo "${src_dirs} - Source directory does not exist."; }
-[[ ! -d ${dst_dirs} ]] && { echo "${dst_dirs} - Destination directory does not exist."; exit 1; }
+    [[ ! -d ${src_dirs} ]] && { echo "${src_dirs} - Source directory does not exist."; exit 1; }
+    [[ ! -d ${dst_dirs} ]] && { echo "${dst_dirs} - Destination directory does not exist."; exit 1; }
 }
 
 # Accept $src_dirs, $dst_dirs. Return void
@@ -138,14 +129,20 @@ convert() {
 	    dst_files=$(echo "${dst_dirs}${src_files[$i]/${src_root}}") # Strip ${src_root}
 	    dst_files="${dst_files/.${comp_type}/.${html_type}}" # Replace file suffix
 	    if [[ ! -f "${dst_files}" ]]; then
-		if [[ ! $(bzcat "${src_files[$i]}") =~ ^.so ]]; then # Skip stub manpages
-		    echo -e "Converting ${src_files[$i]} to ${dst_files}"
-#		    bzcat "${src_files[$i]}" | man2html ${m2h_arg} > "${dst_files}"
-		else
-		    echo -e "Skipping stub \033[1m${src_files[$i]##*/}\033[m"
-		fi
+		case ${gen_stub} in
+		    0) # Skip stub manpages
+			if [[ $(bzcat "${src_files[$i]}") =~ ^.so ]]; then
+			    verbose_mode "Skipping stub \033[1m${src_files[$i]##*/}\033[m"
+			else
+			    verbose_mode "Converting ${src_files[$i]} to ${dst_files}"
+#			    bzcat "${src_files[$i]}" | man2html ${m2h_opt} > "${dst_files}" 2>/dev/null
+			fi;;
+		    1)
+			verbose_mode "Converting ${src_files[$i]} to ${dst_files}"
+#			bzcat "${src_files[$i]}" | man2html ${m2h_opt} > "${dst_files}" 2>/dev/null;;
+		esac
 	    else
-		echo -e "Skipping duplicate \033[1m${src_files[$i]##*/}\033[m"
+		verbose_mode "Skipping duplicate \033[1m${src_files[$i]##*/}\033[m"
 	    fi
 	done
 	echo ""
@@ -155,11 +152,6 @@ convert() {
 
 sanity
 args "${@}"
-num_args "${@}"
-
-
-
-#args "${@}"
-#make_dirs "${src_dirs}" "${dst_dirs}"
-#files_array "${src_dirs}"
-#convert "${src_files}"
+make_dirs "${src_dirs}" "${dst_dirs}"
+files_array "${src_dirs}"
+convert "${src_files}"
